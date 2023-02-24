c3e5829445923e58ce07af94a3893b03f2d22d6c
Yony Zarate Paco <yony.zarate96gmail.com>
2023-02-23 20:19:09 -0400

    ejercicio dom de reloj y alarma

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,8 +1,9 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
-import { digitalClock } from "./dom/reloj.js";
+import { digitalClock, alarm } from "./dom/reloj.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("../assets/alarm.wav", "#activar-alarma", "#desactivar-alarma");
 });
