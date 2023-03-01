c41bb354ff6785e332b4f3052ac8945e803c3f62
Yony Zarate Paco <yony.zarate96gmail.com>
2023-02-28 21:09:33 -0400

    ejercicio de eventos del teclado

---
+++ js/dom/teclado.js
@@ -0,0 +1,47 @@
+const d = document;
+let x = 0,
+  y = 0;
+export function moveBall(e, ball, stage) {
+  const $ball = d.querySelector(ball),
+    $stage = d.querySelector(stage),
+    limitsBall = $ball.getBoundingClientRect(),
+    limitsStage = $stage.getBoundingClientRect();
+  console.log(e.keyCode);
+  console.log(e.key);
+  // const move = (direccion) => {};
+
+  switch (e.keyCode) {
+    case 37:
+      e.preventDefault();
+      // move("left");
+      if (limitsBall.left > limitsStage.left) x--;
+      break;
+    case 38:
+      e.preventDefault();
+      // move("up");
+      if (limitsBall.top > limitsStage.top) y--;
+
+      break;
+    case 39:
+      e.preventDefault();
+      // move("right");
+      if (limitsBall.right < limitsStage.right) x++;
+      break;
+    case 40:
+      e.preventDefault();
+      // move("down");
+      if (limitsBall.bottom < limitsStage.bottom) y++;
+
+      break;
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(${x * 10}px,${y * 10}px)`;
+}
+
+export function shortcuts(e) {
+  // console.log(e.type);
+  // console.log(e.key);
+  // console.log(e.keyCode);
+  // console.log(e);
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,9 +1,15 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
+import { moveBall, shortcuts } from "./dom/teclado.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("../assets/alarm.wav", "#activar-alarma", "#desactivar-alarma");
+});
+
+d.addEventListener("keydown", (e) => {
+  shortcuts(e);
+  moveBall(e, ".ball", ".stage");
 });
