4dac0baaf784d07498efc57884f736c2ba0cbeba
Yony Zarate Paco <yony.zarate96gmail.com>
2023-03-21 11:26:34 -0400

    Ejercicio API localStorahe dark/Light

--- js/dom/tema_oscuro.js
+++ js/dom/tema_oscuro.js
@@ -1,20 +1,37 @@
 const d = document;
+const ls = localStorage;
 export default function darkTheme(btn, classDark) {
   const $themebtn = d.querySelector(btn),
     $selectors = d.querySelectorAll("[data-dark]");
-  console.log($selectors);
+  // console.log($selectors);
   let moon = "🌙",
     sun = "☀️";
+  const lightMode = () => {
+    $selectors.forEach((el) => el.classList.remove(classDark));
+    $themebtn.textContent = moon;
+    ls.setItem("theme", "light");
+  };
+  const darckMode = () => {
+    $selectors.forEach((el) => el.classList.add(classDark));
+    $themebtn.textContent = sun;
+    ls.setItem("theme", "dark");
+  };
   d.addEventListener("click", (e) => {
     if (e.target.matches(btn)) {
-      console.log($themebtn.textContent);
+      // console.log($themebtn.textContent);
       if ($themebtn.textContent === moon) {
-        $selectors.forEach((el) => el.classList.add(classDark));
-        $themebtn.textContent = sun;
+        darckMode();
       } else {
-        $selectors.forEach((el) => el.classList.remove(classDark));
-        $themebtn.textContent = moon;
+        lightMode();
       }
     }
+  });
+  d.addEventListener("DOMContentLoaded", (e) => {
+    // alert("Hola desde la funcion darcktheme");
+    if (ls.getItem("theme") === null) ls.setItem("theme", "light");
+
+    if (ls.getItem("theme") === "light") lightMode();
+
+    if (ls.getItem("theme") === "dark") darckMode();
   });
 }
