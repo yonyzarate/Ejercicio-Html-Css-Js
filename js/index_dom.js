28ac3188bf665849e5c67b3273dcadee3e26b30a
Yony Zarate Paco <yony.zarate96gmail.com>
2023-04-10 20:34:23 -0400

    Ejercicio Video Inteligente (IntersectionObserver & VisibilityChage)

--- js/index_dom.js
+++ js/index_dom.js
@@ -14,6 +14,7 @@ import scrollSpy from "./dom/scroll_espia.js";
 import draw from "./dom/sorteo.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 import darkTheme from "./dom/tema_oscuro.js";
+import smartVideo from "./dom/video_inteligente.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -42,6 +43,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   draw("#winner-btn", ".player");
   slider();
   scrollSpy();
+  smartVideo();
 });
 
 d.addEventListener("keydown", (e) => {
