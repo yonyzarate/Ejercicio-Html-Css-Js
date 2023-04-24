f30e5b357e3a8f2ad99c690e2a545c9cd7ac79a0
Yony Zarate Paco <yony.zarate96gmail.com>
2023-04-24 17:20:06 -0400

    Ejemplo como funciona el API Fetch

--- js/ajax.js
+++ js/ajax.js
@@ -7,10 +7,10 @@
     if (xhr.readyState !== 4) return;
 
     if (xhr.status >= 200 && xhr.status < 300) {
-      console.log("éxito");
+      // console.log("éxito");
 
       let json = JSON.parse(xhr.responseText);
-      console.log(json);
+      // console.log(json);
 
       json.forEach((el) => {
         const $li = document.createElement("li");
@@ -19,11 +19,11 @@
       });
       $xhr.appendChild($fragment);
     } else {
-      console.log("error");
+      // console.log("error");
       let message = xhr.statusText || "Ocurrío un error";
       $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
     }
-    console.log(xhr);
+    // console.log(xhr);
   });
 
   xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
@@ -33,3 +33,35 @@
 // alert("hola mundo");
 // *En este capitulo vimos Ajax objeto XMLHttpRequest como funciona utilizando un api
 // *(jsonpleceholder.typicode.com) que es una api para practicar
+
+// *API Fetch
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    // .then((res) => {
+    //   console.log(res);
+    //   return res.json();
+    // })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      console.log(json);
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email}-- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      console.log(err);
+      let message = err.statusText || "Ocurrío un error";
+      $fetch.innerHTML = `Error ${err.status}: ${message}`;
+    })
+    .finally(() => {
+      console.log(
+        "Esto se ejecutará indepentientemente del resultado de la Pronesa Fetch"
+      );
+    });
+})();
